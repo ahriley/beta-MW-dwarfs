b8730ab5bc016d24ef06e08f031e99e8a61c79cf
ahriley <30327239+ahriley@users.noreply.github.com>
2018-05-03 16:03:02 -0500

    put apostle in graveyard for now, now using elvis PID and UpID
    Former-commit-id: 3b5eddd0a5c61562b07bc014415a57e713835367

--- elvis.py
+++ elvis.py
@@ -1,10 +1,51 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from utils import *
+import utils as u
 
-Mpc_to_km = 3.086*10**19
-km_to_kpc = 10**3/Mpc_to_km
+num = 50
+all, topvmax, topvpeak = [], [], []
+# sim = 'Hall&Oates'
+for sim in u.list_of_sims(suite='elvis'):
+    subs = u.load_elvis(sim=sim)
 
-sim = 'Hall&Oates'
-subs = load_elvis(sim=sim)
+    # identify main halos, separate from rest
+    subs.sort_values('M_dm', ascending=False, inplace=True)
+    haloIDs = list(subs.index.values[0:2])
+    subs, halos = subs.drop(haloIDs), subs.loc[haloIDs]
+    halos.sort_values('M_star', ascending=False, inplace=True)
+    And_id = halos.iloc[0].name
+    MW_id = halos.iloc[1].name
+
+    # grab subhalos of main halos
+    subs = subs[(subs['hostID'] == And_id) | (subs['hostID'] == MW_id)]
+
+    # center on main halos, convert to spherical coordinates
+    subs = u.center_on_hosts(hosts=halos, subs=subs)
+    subs.x, subs.y, subs.z = subs.x*u.Mpc2km, subs.y*u.Mpc2km, subs.z*u.Mpc2km
+    subs = u.compute_spherical_hostcentric_sameunits(df=subs)
+    subs.x, subs.y, subs.z = subs.x*u.km2kpc, subs.y*u.km2kpc, subs.z*u.km2kpc
+    subs.r = subs.r*u.km2kpc
+
+    Andsubs = subs[subs.hostID == And_id]
+    MWsubs = subs[subs.hostID == MW_id]
+
+    all.append([u.beta(Andsubs), u.beta(MWsubs)])
+    topvmax.append([u.beta(Andsubs.nlargest(n=num, columns='Vmax')), u.beta(MWsubs.nlargest(n=num, columns='Vmax'))])
+    topvpeak.append([u.beta(Andsubs.nlargest(n=num, columns='Vpeak')), u.beta(MWsubs.nlargest(n=num, columns='Vpeak'))])
+
+all = np.array(all).flatten()
+topvmax = np.array(topvmax).flatten()
+topvpeak = np.array(topvpeak).flatten()
+
+fulldata = np.concatenate((all, topvmax, topvpeak))
+edges = np.linspace(min(fulldata), max(fulldata), 100)
+
+kwargs = {'histtype':'step', 'cumulative':True, 'bins':edges, 'lw':2.0, 'density':True}
+plt.hist(all, **kwargs, label='allsubs')
+plt.hist(topvmax, **kwargs, label='top 50 vmax')
+plt.hist(topvpeak, **kwargs, label='top 50 vpeak')
+plt.title("CDF of beta for subhalos in ELVIS")
+plt.xlabel('Beta')
+plt.ylabel('Fraction of main halos')
+plt.legend(loc='upper left');
--- utils.py
+++ utils.py
@@ -2,6 +2,11 @@ import numpy as np
 import pandas as pd
 import glob
 
+Mpc2km = 3.086*10**19
+km2kpc = 10**3/Mpc2km
+
+h = 0.71    # ELVIS
+
 def beta(df):
     return 1-(np.var(df.v_theta)+np.var(df.v_phi))/(2*np.var(df.v_r))
 
@@ -26,11 +31,19 @@ def compute_spherical_hostcentric_sameunits(df):
     v_r = (x*vx + y*vy + z*vz) / r
     v_theta = r*((z*(x*vx+y*vy)-vz*(x**2+y**2)) / (r**2*np.sqrt(x**2+y**2)))
     v_phi = r*np.sin(theta)*((x*vy - y*vx) / (x**2 + y**2))
+    v_t = np.sqrt(v_theta**2 + v_phi**2)
+
+    # check that coordinate transformation worked
+    cart = np.sqrt(vx**2 + vy**2 + vz**2)
+    sphere = np.sqrt(v_r**2 + v_theta**2 + v_phi**2)
+    sphere2 = np.sqrt(v_r**2 + v_t**2)
+    assert np.isclose(cart, sphere).all()
+    assert np.isclose(cart, sphere2).all()
 
     df2 = df.copy()
     df2['r'], df2['theta'], df2['phi'] = r, theta, phi
     df2['v_r'], df2['v_theta'], df2['v_phi'] = v_r, v_theta, v_phi
-    df2['v_t'] = np.sqrt(v_theta**2 + v_phi**2)
+    df2['v_t'] = v_t
 
     return df2
 
@@ -45,43 +58,43 @@ def list_of_sims(suite):
     else:
         raise ValueError("suite must be 'elvis' or 'apostle'")
 
-def load_apostle(sim):
-    filename = 'data/apostle/'+sim+'_subs.pkl'
-    df = pd.read_pickle(filename)
-    return df.drop_duplicates()
-
 def load_elvis(sim):
     filename = 'data/elvis/'+sim+'.txt'
 
     # read in the data
     with open(filename) as f:
-        ID, x, y, z, vx, vy, vz, mvir, mstar = [[] for i in range(9)]
+        id, x, y, z, vx, vy, vz, vmax, vpeak, mvir = [[] for i in range(10)]
+        mpeak, rvir, rmax, apeak, mstar, mstar_b = [[] for i in range(6)]
+        npart, pid, upid = [], [], []
         for line in f:
             if line[0] == '#':
                 continue
             items = line.split()
-            ID.append(int(items[0]))
+            id.append(int(items[0]))
             x.append(float(items[1]))
             y.append(float(items[2]))
             z.append(float(items[3]))
             vx.append(float(items[4]))
             vy.append(float(items[5]))
             vz.append(float(items[6]))
+            vmax.append(float(items[7]))
+            vpeak.append(float(items[8]))
             mvir.append(float(items[9]))
+            mpeak.append(float(items[10]))
+            rvir.append(float(items[11]))
+            rmax.append(float(items[12]))
+            apeak.append(float(items[13]))
             mstar.append(float(items[14]))
-    df = {'M_dm': mvir, 'M_star': mstar, 'x': x, 'y': y, 'z': z,
-            'vx': vx, 'vy': vy, 'vz': vz}
-    df = pd.DataFrame(df, index=ID)
+            mstar_b.append(float(items[15]))
+            npart.append(int(items[16]))
+            pid.append(int(items[17]))
+            upid.append(int(items[18]))
+
+    # convert to pandas format
+    df = {'PID': pid, 'hostID': upid, 'npart': npart, 'apeak': apeak,
+            'M_dm': mvir, 'M_star': mstar, 'Mpeak': mpeak, 'Mstar_b': mstar_b,
+            'Vmax': vmax, 'Vpeak': vpeak, 'Rvir': rvir, 'Rmax': rmax,
+            'x': x, 'y': y, 'z': z, 'vx': vx, 'vy': vy, 'vz': vz}
+    df = pd.DataFrame(df, index=id)
     df.index.name = 'ID'
-
-    # main halo association just based on distance
-    host1 = df.iloc[0]
-    host2 = df.iloc[1]
-    df['d1'] = np.sqrt((df.x-host1.x)**2+(df.y-host1.y)**2+(df.z-host1.z)**2)
-    df['d2'] = np.sqrt((df.x-host2.x)**2+(df.y-host2.y)**2+(df.z-host2.z)**2)
-    df['hostID'] = -1
-    df.loc[df['d1'] < df['d2'], 'hostID'] = host1.name
-    df.loc[df['d1'] > df['d2'], 'hostID'] = host2.name
-    df.drop(columns=['d1', 'd2'], inplace=True)
-
     return df
