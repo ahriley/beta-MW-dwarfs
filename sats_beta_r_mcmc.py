952681776499d07a450fc486af82911bd359f3b4
ahriley <30327239+ahriley@users.noreply.github.com>
2018-05-10 00:54:12 -0500

    also saving MC coordinate transformations
    Former-commit-id: 59835c72f092897315853d75afa25dd4756c6f45

--- sats_beta_r_mcmc.py
+++ sats_beta_r_mcmc.py
@@ -10,7 +10,7 @@ import pickle
 
 study = 'fritz'
 plot = False
-n = 2000
+n = 10000
 edges = np.arange(0,250+1,50)
 rvals = (edges[1:] + edges[:-1]) / 2
 head = str(edges)
@@ -45,17 +45,19 @@ print("Generated MCMC samplings")
 positions = np.array(positions)
 assert positions.shape == (len(names), n, 4)
 
+# for each sample, compute beta
 beta_profile = []
+coords_converted = []
 status = 0.01
 print("Converting SkyCoords, binning by r, computing beta")
 for i in range(n):
-    print(i+1)
     if i/n >= status:
-        print("{0:.0f}%".format(i/b * 100))
+        print("{0:.0f}%".format(i/n * 100))
         status += 0.01
 
     pos_i = positions[:,i,]
 
+	# convert each dwarf from celestial to cartesian
     x,y,z,vx,vy,vz = [[] for i in range(6)]
     for name, pos in zip(names, pos_i):
         dsph = dwarfs[name]
@@ -76,11 +78,14 @@ for i in range(n):
     df = {'x': x, 'y': y, 'z': z, 'vx': vx, 'vy': vy, 'vz': vz}
     df = pd.DataFrame(df, index=names)
 
+    # coordinate conversion, save for later
     df.x, df.y, df.z = df.x*utils.Mpc2km, df.y*utils.Mpc2km, df.z*utils.Mpc2km
     df = utils.compute_spherical_hostcentric_sameunits(df=df)
     df.x, df.y, df.z = df.x*utils.km2kpc, df.y*utils.km2kpc, df.z*utils.km2kpc
     df.r = df.r*utils.km2kpc
+    coords_converted.append(df.values)
 
+	# compute beta in different radial bins
     df = df[(df.r < 250) & (df.index != 'Cra I')]
     cut = pd.cut(df['r'], bins=edges, labels=False)
     beta_df = np.empty(0)
@@ -88,6 +93,15 @@ for i in range(n):
         beta_df = np.append(beta_df, utils.beta(df[cut == i]))
     beta_profile.append(beta_df)
 beta_profile = np.array(beta_profile)
+coords_converted = np.array(coords_converted)
 
+# save both the MC sampling and the computed profiles
 np.savetxt('data/mcmc/beta_profile_'+study+'.txt', beta_profile, header=head)
+
+# SHAPE: 39 dwarfs, 10000 samplings, 4 params
+# ORDER: mu_alpha, mu_delta, vel_los, dist
 np.save('data/mcmc/sampling_'+study, positions, allow_pickle=False)
+
+# SHAPE: 10000 samplings, 39 dwarfs, 13 params
+# ORDER: vx vy vz x y z r theta phi v_r v_theta v_phi v_t
+np.save('data/mcmc/sampling_converted_'+study, coords_converted, allow_pickle=False)
