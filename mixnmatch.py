1e8e440e22c614784173762f34e853e50e5c6fa4
ahriley <30327239+ahriley@users.noreply.github.com>
2018-07-27 16:02:07 -0500

    add LMC/SMC, rename "HPMKS" to "gold"
    Former-commit-id: 9d16247916adfe3d80178d062b2f1d2fdf769a7c

--- mixnmatch.py
+++ mixnmatch.py
@@ -6,7 +6,7 @@ studies = ['helmi', 'pace', 'massari', 'kallivayalil', 'simon']
 names = []
 study_from = []
 MC_set = []
-
+magclouds = []
 for study in studies:
     dwarf_file = 'data/dwarfs/'+study+'.yaml'
     with open(dwarf_file, 'r') as f:
@@ -19,6 +19,9 @@ for study in studies:
             names.append(name)
             study_from.append(study)
             MC_set.append(MC_dwarfs[study_names.index(name)])
+
+            if name == 'LMC' or name == 'SMC':
+                magclouds.append(MC_dwarfs[study_names.index(name)])
 MC_set = np.array(MC_set)
 
 fritz_file = 'data/dwarfs/fritz.yaml'
@@ -26,17 +29,23 @@ with open(fritz_file, 'r') as f:
     fritz = yaml.load(f)
 fritz = list(fritz.keys())
 
-assert np.isin(names, fritz).all()
 MC_dwarfs = np.load('data/sampling/fritz_converted.npy')
 MC_set_fritz = []
 for name in names:
-    MC_set_fritz.append(MC_dwarfs[fritz.index(name)])
+    try:
+        MC_set_fritz.append(MC_dwarfs[fritz.index(name)])
+    except:
+        # Magellanic Clouds not in Fritz
+        continue
 
-np.save('data/sampling/HPMKS_converted', MC_set)
-np.save('data/sampling/fritz_HPMKS_converted', MC_set_fritz)
+MC_set_fritz = np.concatenate((MC_set_fritz, np.array(magclouds)))
+assert MC_set_fritz.shape == MC_set.shape
+
+np.save('data/sampling/gold_converted', MC_set)
+np.save('data/sampling/fritz_gold_converted', MC_set_fritz)
 
 map = {'name': names, 'study': study_from}
-with open('data/sampling/HPMKS_key.pkl', 'wb') as f:
+with open('data/sampling/gold_key.pkl', 'wb') as f:
     pickle.dump(map, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 """
